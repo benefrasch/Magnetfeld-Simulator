92cb9265158936bbcfd4171c3a2e359442c4dae8
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 21:43:04 +0100

    added time logging

--- Magnetfeld Simulator/Constants.h
+++ Magnetfeld Simulator/Constants.h
@@ -6,7 +6,7 @@
 	static const float radius = 0.04;
 	static const float step_size = 0.01;
 
-	static const float Current = 10;
+	static const float Current = 1000;
 	static const long double mu0 = 1.25663706212e-06; //magnetische Feldkonstante
 	const long double PI = 3.141592653589793238463; //evtl. ug. falls ungenau, mehr dezimalen benötigt
 
--- Magnetfeld Simulator/Magnetfeld Simulator.cpp
+++ Magnetfeld Simulator/Magnetfeld Simulator.cpp
@@ -6,6 +6,7 @@
 #include "Constants.h"
 #include <Windows.h>
 #include <future>
+#include <chrono>
 
 static BMP bmp(image_size_x, image_size_y, false);
 static int done_graphs = 0;
@@ -14,17 +15,21 @@ static int done_graphs = 0;
 bool plot_vector(std::vector<wire_part>* parts, long double y_start, bool inverted) {
 	long double x = simulation_size_x / 2,
 		y = y_start;
-
-	while (x >= simulation_size_x / 2 && x <= simulation_size_x) {
-		Vektor b_vektor = B_Vektor::vektor(parts, x, y, simulation_size_z / 2);
-		if (inverted)
+	if (!inverted)
+		while (x <= simulation_size_x / 2 && x >= 0) {
+			Vektor b_vektor = B_Vektor::vektor(parts, x, y, simulation_size_z / 2);
+			x += b_vektor.x, y += b_vektor.y;
+			if (!(x<0 || x>simulation_size_x || y<0 || y>simulation_size_y))
+				bmp.set_pixel(x * (image_size_x / simulation_size_x), y * (image_size_y / simulation_size_y), 255, 255, 255, 255);
+		}
+	else
+		while (x >= simulation_size_x / 2 && x <= simulation_size_x) {
+			Vektor b_vektor = B_Vektor::vektor(parts, x, y, simulation_size_z / 2);
 			b_vektor.x *= -1, b_vektor.y *= -1, b_vektor.z *= -1;
-
-		x += b_vektor.x, y += b_vektor.y;
-
-		if (!(x<0 || x>simulation_size_x || y<0 || y>simulation_size_y))
-			bmp.set_pixel(x * (image_size_x / simulation_size_x), y * (image_size_y / simulation_size_y), 255, 255, 255, 255);
-	}
+			x += b_vektor.x, y += b_vektor.y;
+			if (!(x<0 || x>simulation_size_x || y<0 || y>simulation_size_y))
+				bmp.set_pixel(x * (image_size_x / simulation_size_x), y * (image_size_y / simulation_size_y), 255, 255, 255, 255);
+		}
 	bmp.write("output.bmp");
 	++done_graphs;
 	return true;
@@ -32,18 +37,17 @@ bool plot_vector(std::vector<wire_part>* parts, long double y_start, bool invert
 
 int main()
 {
+	auto start = std::chrono::high_resolution_clock::now();
 
 	bmp.set_pixel((simulation_size_x / 2) * (image_size_x / simulation_size_x), (float)(simulation_size_y / 2 - radius) * (image_size_y / simulation_size_y), 255, 0, 0, 255);
 	bmp.set_pixel((simulation_size_x / 2) * (image_size_x / simulation_size_x), (float)(simulation_size_y / 2 + radius) * (image_size_y / simulation_size_y), 255, 0, 0, 255);
 	std::vector<wire_part> parts = Wire_parts::ring();
 
-	std::vector<double> start_y_values;
-	for (long double y_start = simulation_size_y / 2 - radius + 0.01; y_start <= (double)simulation_size_y / 2 + radius - 0.01; y_start += step_size) {
+	std::vector<long double> start_y_values;
+	for (long double y_start = (double)simulation_size_y / 2 - radius + 0.01; y_start <= (double)simulation_size_y / 2 + radius - 0.01; y_start += step_size) {
 		Vektor b_vektor = B_Vektor::vektor(&parts, simulation_size_x / 2, y_start, simulation_size_z / 2);
-		std::cout << y_start << "   " << b_vektor.x << "\n";
-		for (long double y_value = y_start; y_value < y_start + step_size; y_value += (long double)1 / (-1000000 * b_vektor.x)) {
+		for (long double y_value = y_start; y_value < y_start + step_size; y_value += (long double)1 / (-2000000 * b_vektor.x)) { //adjusts itaration size so there are more start points for a stronger field
 			start_y_values.push_back(y_value);
-			std::cout << y_value << "\n";
 		}
 	}
 
@@ -61,5 +65,10 @@ int main()
 		std::cout << "Waiting for calculations to finish - " << done_graphs << " out of " << (start_y_values.size() * 2) << " done!\n";
 		Sleep(10000);
 	}
+
+	auto finish = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = finish - start;
+
+	std::cout << "all done! Only took " << (float)elapsed.count()/60 << " minutes.";
 }
 
