a4f52e00603efeecb00cf53e9f192dfd2b6889f6
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 18:20:21 +0100

    die Werte ergeben sinn!!!!!

--- Magnetfeld Simulator/Constants.h
+++ Magnetfeld Simulator/Constants.h
@@ -4,10 +4,11 @@
 
 	static const int simulation_wire_parts_count = 20;
 	static const float radius = 0.04;
+	static const float step_size = 0.01;
 
-	static const float Current = 1;
+	static const float Current = 10;
 	static const long double mu0 = 1.25663706212e-06; //magnetische Feldkonstante
 	const long double PI = 3.141592653589793238463; //evtl. ug. falls ungenau, mehr dezimalen benötigt
 
-	static const int image_size_x = 1000;
-	static const int image_size_y = 1000;
+	static const int image_size_x = 2000;
+	static const int image_size_y = 2000;
--- Magnetfeld Simulator/Magnetfeld Simulator.cpp
+++ Magnetfeld Simulator/Magnetfeld Simulator.cpp
@@ -4,28 +4,58 @@
 #include "Wire_parts.h"
 #include "B_Vektor.h"
 #include "Constants.h"
-
+#include <Windows.h>
 
 static BMP bmp(image_size_x, image_size_y, false);
 
 int main()
 {
-
-	long double x = 0.5, 
-				y = 0.48;
-
-	bmp.set_pixel((simulation_size_x / 2)*(image_size_x / simulation_size_x), (float)(simulation_size_y / 2 - radius)*(image_size_y / simulation_size_y), 255, 0, 0, 255);
-	bmp.set_pixel((simulation_size_x / 2)*(image_size_x / simulation_size_x), (float)(simulation_size_y / 2 + radius)*(image_size_y / simulation_size_y), 255, 0, 0, 255);
-
-	std::vector<wire_part> parts = Wire_parts::ring();
-	for (int i = 0; i < 1000; ++i) {
-		Vektor b_vektor = B_Vektor::vektor(&parts,x, y, simulation_size_z/2);
-		x += b_vektor.x; y += b_vektor.y;
-		bmp.set_pixel(x * (image_size_x / simulation_size_x), y * (image_size_y / simulation_size_y), 255, 255, 255, 255);
-		//std::cout << "\n" << b_vektor.x << "	" << b_vektor.y << "	" << b_vektor.z << "		" << x << "	" << y;
-	}
-	bmp.write("output.bmp");
-
 	
+
+
+
+	bmp.set_pixel((simulation_size_x / 2) * (image_size_x / simulation_size_x), (float)(simulation_size_y / 2 - radius) * (image_size_y / simulation_size_y), 255, 0, 0, 255);
+	bmp.set_pixel((simulation_size_x / 2) * (image_size_x / simulation_size_x), (float)(simulation_size_y / 2 + radius) * (image_size_y / simulation_size_y), 255, 0, 0, 255);
+	std::vector<wire_part> parts = Wire_parts::ring();
+
+	std::vector<double> start_y_values;
+	for (float y_start = simulation_size_y / 2 - radius + 0.01; y_start <= simulation_size_y / 2 + radius - 0.01; y_start += step_size) {
+		Vektor b_vektor = B_Vektor::vektor(&parts, simulation_size_x/2, y_start, simulation_size_z / 2);
+		std::cout << y_start << "   " << b_vektor.x << "\n";
+		for (float y_value = y_start; y_value < y_start + step_size; y_value +=  (long double)1/(-1000000*b_vektor.x)  ) {
+			start_y_values.push_back(y_value);
+			std::cout << y_value << "\n";
+		}
+	}
+
+	std::cout << "\n starting calculation...\n\n";
+
+	for (double y_start : start_y_values)
+	{
+		std::cout << y_start << "\n";
+		long double x = simulation_size_x / 2,
+					y = y_start;
+
+		while (x <= simulation_size_x / 2 && x >= 0) {
+			Vektor b_vektor = B_Vektor::vektor(&parts, x, y, simulation_size_z / 2);
+			x += b_vektor.x; y += b_vektor.y;
+			if (!(x<0 || x>simulation_size_x || y<0 || y>simulation_size_y))
+				bmp.set_pixel(x * (image_size_x / simulation_size_x), y * (image_size_y / simulation_size_y), 255, 255, 255, 255);
+			//std::cout << "\n" << b_vektor.x << "	" << b_vektor.y << "	" << b_vektor.z << "		" << x << "	" << y;
+		}
+		x = simulation_size_x / 2,
+		y = y_start;
+		while (x >= simulation_size_x / 2 && x <= simulation_size_x) {
+			Vektor b_vektor = B_Vektor::vektor(&parts, x, y, simulation_size_z / 2);
+			b_vektor.x *= -1; b_vektor.y *= -1; b_vektor.z *= -1;
+			x += b_vektor.x; y += b_vektor.y;
+			if (!(x<0 || x>simulation_size_x || y<0 || y>simulation_size_y))
+				bmp.set_pixel(x * (image_size_x / simulation_size_x), y * (image_size_y / simulation_size_y), 255, 255, 255, 255);
+			//std::cout << "\n" << b_vektor.x << "	" << b_vektor.y << "	" << b_vektor.z << "		" << x << "	" << y;
+		}
+		bmp.write("output.bmp");
+	}
+	
+
 }
 
